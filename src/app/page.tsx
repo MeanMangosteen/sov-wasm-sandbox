a6d28cd6a30073f7cc57a6d81d08fcf74537368a
Aashwin Varshney <aashwin.varshney@gmail.com>
2025-05-01 17:11:12 +1000

    wip

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,27 @@
-import { App } from "./App";
+import { createStandardRollup } from "@sovereign-sdk/web3";
 import styles from "./page.module.css";
+// import { Asset } from "@bulletxyz/bullet-sdk";
+
+export const dynamic = "force-dynamic";
 
 export default function Home() {
+  createStandardRollup({
+    url: "https://rollup-testnet.zeta.markets",
+    context: {
+      defaultTxDetails: {
+        max_priority_fee_bips: 0,
+        max_fee: "100000000",
+        gas_limit: null,
+        chain_id: 4321,
+      },
+    },
+  });
+
+  // console.log(Asset);
+
   return (
     <div className={styles.page}>
-      <App />
+      <h1>Hello World</h1>
     </div>
   );
 }
